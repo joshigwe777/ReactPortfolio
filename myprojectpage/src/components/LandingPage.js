70b7fb49678258b4b790d03772d407f614d84634
joshigwe777 <39813342+joshigwe777@users.noreply.github.com>
2022-03-31 15:18:28 -0400

    starter code

---
+++ myprojectpage/src/components/LandingPage.js
@@ -0,0 +1,10 @@
+function LandingPage() {
+    return <>
+    <div>
+        <h3>Hello my name is Joshua and I like making cool stuff</h3>
+        <img alt="profile picture of Joshua"/>
+    </div>
+    </>
+
+}
+export default LandingPage;
---
+++ myprojectpage/src/components/SocialMediaPanel.js
@@ -0,0 +1,5 @@
+function SocialMediaPanel() {
+    return <>
+    </>
+}
+export default SocialMediaPanel;
