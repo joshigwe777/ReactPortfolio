f70e5bbdb0fa0ca11d43bb2d5d5da4bd1c1ada28
joshigwe777 <39813342+joshigwe777@users.noreply.github.com>
2022-04-02 19:05:34 -0400

    fonts, color and picture

--- myprojectpage/src/components/LandingPage.js
+++ myprojectpage/src/components/LandingPage.js
@@ -2,7 +2,7 @@ function LandingPage() {
     return <>
     <div className="mx-auto">
         <h3 className="text-center my-5">Hello my name is Joshua and I like making cool stuff</h3>
-        <img alt="profile picture of Joshua"/>
+        <img className="mx-auto rounded d-block" src="images/profile.jpg" alt="profile picture of Joshua"/>
     </div>
     </>
 
