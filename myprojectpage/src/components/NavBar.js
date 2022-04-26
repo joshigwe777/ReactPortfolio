572c172c345177c6a97a9f0c9e16eee870986f2e
joshigwe777 <39813342+joshigwe777@users.noreply.github.com>
2022-04-26 15:17:45 -0400

    more styling adjustments

--- myprojectpage/src/components/NavBar.js
+++ myprojectpage/src/components/NavBar.js
@@ -2,12 +2,27 @@ import { Link } from "react-router-dom";
 import logo from "../images/logo.png"
 function NavBar() {
     return <>
-        <nav className="navbar navbar-expand-lg ">
-            <Link to="/myprojectpage"><img className="logo mr-auto" src={logo} /></Link>
-            <Link to="/myprojectpage" className="ml-auto nav-link">Home</Link>
-            <Link to="/myprojectpage/portfolio" class="nav-link">Portfolio</Link>
-            <Link to="/myprojectpage/aboutMe" class="nav-link">About Me</Link>
-            <Link to="/myprojectpage/contactForm" class="nav-link">Contact</Link>
+        <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+            <a className="navbar-brand"><Link to="/myprojectpage"><img className="logo mr-auto" src={logo} /></Link></a>
+            <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                    <li className="nav-item active">
+                        <a className="nav-link"><Link to="/myprojectpage" className="nav-link">Home</Link></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link"><Link to="/myprojectpage/portfolio" className="nav-link">Portfolio</Link></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link"><Link to="/myprojectpage/aboutMe" className="nav-link">About Me</Link></a>
+                    </li>
+                    <li className="nav-item">
+                        <a className="nav-link"><Link to="/myprojectpage/contactForm" className="nav-link">Contact</Link></a>
+                    </li>
+                </ul>
+            </div>
 
         </nav>
     </>
