d091c57dd6a591806223914a7aa2434fbd3726ba
joshigwe777 <39813342+joshigwe777@users.noreply.github.com>
2022-08-08 14:38:11 -0400

    bug fixes before eploy

--- myprojectpage/src/components/Footer.js
+++ myprojectpage/src/components/Footer.js
@@ -10,7 +10,6 @@ function Footer() {
                     <a href="https://github.com/joshigwe777" target="_blank"><i className="fa-brands mx-3 fa-2xl fa-github"></i></a>
                     <a href="https://www.linkedin.com/in/joshua-igwe-229ba99a/" target="_blank"><i className="fa-brands mx-3 fa-2xl fa-linkedin"></i></a>
                     <a href="https://twitter.com/Joshua__Igwe" target="_blank"><i className="fa-brands fa-2xl mx-3 fa-twitter"></i></a>
-                    <a><i className="fa-brands fa-2xl mx-3 fa-youtube" target="_blank"></i></a>
                 </nav>
             </div>
         </footer>
