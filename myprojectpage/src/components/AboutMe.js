8ddde72e549d36d7c207464d6548ff102ce0350a
joshigwe777 <39813342+joshigwe777@users.noreply.github.com>
2023-02-05 20:03:35 -0500

    deployed

--- myprojectpage/src/components/AboutMe.js
+++ myprojectpage/src/components/AboutMe.js
@@ -8,9 +8,12 @@ function AboutMe() {
                 </div>
                 <div className="col-7">
                     <h3 className="">Who am I?</h3>
-                    <p>I am a former Industrial and Systems Engineering major who fell in love with
-                        programming my senior year of college. After working for a few years in the non-profit space I made my transition into software development.
-                        I love the creative and engineering process and have a passion for creating beaitoful applications that improve the lives of others. I am looking forward to a long career of unending opportunities to learn and apply new skills to the world's most important problems. </p>
+                    <p>As a former Industrial and Systems Engineering major, I developed a passion for programming during my senior year of college. 
+                        After honing my skills in the non-profit sector, I smoothly transitioned into software development. 
+                        I bring experience in both backend and test engineering, utilizing technologies such as Java, Spring, Oracle, MySql, Docker, AWS, and more. 
+                        Additionally, I am familiar with front-end technologies including Javascript, React, HTML, and CSS. 
+                        To continue my growth as an engineer, I am participating in the Formation Fellowship, where I am honing my computer science and systems design fundamentals, and receiving mentorship from engineers at top tech companies. 
+                        When I am not absorbed in software, I enjoy playing and watching basketball, meditating, reading, listening to podcasts, and sipping coffee at local coffee shops.  </p>
                 </div>
 
             </div>
