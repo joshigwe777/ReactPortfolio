e0b87b12435217ee8043d6bbcee1d96e859ce0c6
joshigwe777 <joshigwe777@gmail.com>
2023-11-13 18:49:21 -0500

    updating styles

--- myprojectpage/src/components/NavBar.js
+++ myprojectpage/src/components/NavBar.js
@@ -6,18 +6,18 @@ import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 function NavBar() {
     return <>
-        <Navbar className="mb-5" bg="dark" expand="md">
+        <Navbar className="mb-5" expand="md">
             <Container>
                 <Navbar.Brand href="#home"><Link to="/"><img className="logo mr-auto" src={logo} /></Link></Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
-                    <Nav className="me-auto">
-                        <Nav.Link href="/">Home</Nav.Link>
-                        <Nav.Link href="/myprojectpage/portfolio">Portfolio</Nav.Link>
-                        <Nav.Link href="/myprojectpage/experience">Experience</Nav.Link>
-                        <Nav.Link href="/myprojectpage/aboutMe">About Me</Nav.Link>
-                        <Nav.Link href="/myprojectpage/contactForm">Contact</Nav.Link>
-                        <Nav.Link href="/myprojectpage/experience">Experience</Nav.Link>
+                    <Nav className="me-auto navs">
+                        <Nav.Link style={{ color: 'white' }} href="/">Home</Nav.Link>
+                        <Nav.Link style={{ color: 'white' }} href="/myprojectpage/portfolio">Portfolio</Nav.Link>
+                        <Nav.Link style={{ color: 'white' }} href="/myprojectpage/experience">Experience</Nav.Link>
+                        <Nav.Link style={{ color: 'white' }} href="/myprojectpage/aboutMe">About Me</Nav.Link>
+                        <Nav.Link style={{ color: 'white' }} href="/myprojectpage/contactForm">Contact</Nav.Link>
+                        <Nav.Link  style={{ color: 'white' }}href="/myprojectpage/experience">Experience</Nav.Link>
                     </Nav>
                 </Navbar.Collapse>
             </Container>
