496d328b7cb7bdebdefafcc9351113dbfff4b4e9
joshigwe777 <joshigwe777@gmail.com>
2023-11-16 15:34:08 -0500

    added animations

--- myprojectpage/src/components/Experience.js
+++ myprojectpage/src/components/Experience.js
@@ -4,17 +4,20 @@ export default function Experience() {
 
     return <>
         <div className="text-center">
-        <h2 style={{ borderBottom: '2px solid white', display: 'inline', paddingBottom: '-3px' }}>Experience</h2>
-            <div className="chase mb-3 mt-5">
-                <h4>Company: JP Morgan Chase</h4>
-                <h4>Role: Software Engineer</h4>
-                <h4>Duration: May 2023 - Present</h4>
+        <h2>Experience</h2>
+            <div className="experiences">
+            <div className="chase">
+                <h5>Company: JP Morgan Chase</h5>
+                <h5>Role: Software Engineer</h5>
+                <h5>Duration: May 2023 - Present</h5>
             </div>
-            <div className="dev10 mb-5" >
-                <h4>Company: Genesis10</h4>
-                <h4>Role: Software Developer Consultant</h4>
-                <h4>Duration: October 2021 - April 2023</h4>
+            <div className="dev10" >
+                <h5>Company: Genesis10</h5>
+                <h5>Role: Software Developer Consultant</h5>
+                <h5>Duration: October 2021 - April 2023</h5>
             </div>
+            </div>
+            
         </div>
     
     
--- myprojectpage/src/components/NavBar.js
+++ myprojectpage/src/components/NavBar.js
@@ -14,7 +14,6 @@ function NavBar() {
                     <Nav className="me-auto navs">
                         <Nav.Link style={{ color: 'white' }} href="/">Home</Nav.Link>
                         <Nav.Link style={{ color: 'white' }} href="/myprojectpage/portfolio">Portfolio</Nav.Link>
-                        <Nav.Link style={{ color: 'white' }} href="/myprojectpage/experience">Experience</Nav.Link>
                         <Nav.Link style={{ color: 'white' }} href="/myprojectpage/aboutMe">About Me</Nav.Link>
                         <Nav.Link style={{ color: 'white' }} href="/myprojectpage/contactForm">Contact</Nav.Link>
                         <Nav.Link  style={{ color: 'white' }}href="/myprojectpage/experience">Experience</Nav.Link>
